7c0700dd59ba499c26ae1501d022418b2ec412a8
xeizer <keiser.form@gmail.com>
2023-03-02 11:47:43 +0700

    CRUD BARANG

--- app/Http/Livewire/Barang.php
+++ app/Http/Livewire/Barang.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Semuabarang;
 use Livewire\Component;
 
 class Barang extends Component
 {
+    public $barcode;
+    public $namabarang;
+    public $harga;
+    public $stok;
+    public $barangyangakandiedit;
+
+    public function simpan()
+    {
+        if ($this->barangyangakandiedit) {
+            $simpan = $this->barangyangakandiedit;
+        } else {
+            $simpan = new Semuabarang();
+        }
+        $simpan->barcode = $this->barcode;
+        $simpan->nama_barang = $this->namabarang;
+        $simpan->harga = $this->harga;
+        $simpan->stok = $this->stok;
+        $simpan->save();
+        $this->reset();
+    }
+    public function hapus($id)
+    {
+        Semuabarang::find($id)->delete();
+    }
+    public function edit($id)
+    {
+        $this->barangyangakandiedit = Semuabarang::find($id);
+        $this->barcode = $this->barangyangakandiedit->barcode;
+        $this->namabarang = $this->barangyangakandiedit->nama_barang;
+        $this->harga = $this->barangyangakandiedit->harga;
+        $this->stok = $this->barangyangakandiedit->stok;
+    }
     public function render()
     {
-        return view('livewire.barang');
+        return view('livewire.barang')->with([
+            'semuabarang' => Semuabarang::all()
+        ]);
     }
 }
--- resources/views/livewire/barang.blade.php
+++ resources/views/livewire/barang.blade.php
@@ -5,7 +5,7 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    <h6>DATA BARANG TOKO SUKSES SELALU OGAH RUGI</h6>
+                    <h6>DATA BARANG TOKO SUKSES SELALU</h6>
                     <label>BARCODE</label>
                     <input type="text" class="form-control" wire:model='barcode' />
                     <label>NAMA BARANG</label>
@@ -14,7 +14,7 @@
                     <input type="text" class="form-control" wire:model='harga' />
                     <label>STOK</label>
                     <input type="text" class="form-control" wire:model='stok' />
-                    <button class="btn btn-primary">SIMPAN</button>
+                    <button class="btn btn-primary" wire:click='simpan'>SIMPAN</button>
                     <hr />
                     <table class="table table-bordered">
                         <tr>
@@ -24,6 +24,19 @@
                             <th>STOK</th>
                             <th>EDIT/HAPUS</th>
                         </tr>
+                        @foreach ($semuabarang as $item)
+                            <tr>
+                                <td>{{ $item->barcode }}</td>
+                                <td>{{ $item->nama_barang }}</td>
+                                <td>Rp. {{ number_format($item->harga) }}</td>
+                                <td>{{ $item->stok }}</td>
+                                <td>
+                                    <button class="btn btn-danger"
+                                        wire:click="hapus({{ $item->id }})">HAPUS</button>
+                                    <button class="btn btn-warning" wire:click="edit({{ $item->id }})">EDIT</button>
+                                </td>
+                            </tr>
+                        @endforeach
                     </table>
                 </div>
             </div>
